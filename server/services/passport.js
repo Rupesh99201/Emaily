67d03ae7d3f0fdb4d0926b47e2b5ef91acd21961
Rupesh <rupesh99201@gmail.com>
2020-08-12 11:06:27 +0530

    new changes added

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,12 +6,14 @@ const keys = require("../config/keys");
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
+  console.log("serialuser", user.id);
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then((user) => {
     done(null, user);
+    console.log("deserialize user", user);
   });
 });
 
@@ -25,12 +27,14 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
-          console.log("Existing User", profile.id);
+          console.log("exiting User", existingUser);
+          done(null, existingUser);
         } else {
-          const user = new User({ googleId: profile.id })
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id })
             .save()
             .then((user) => done(null, user));
-          console.log("Profile ID", profile.id);
+          // console.log("new User", user);
         }
       });
     }
